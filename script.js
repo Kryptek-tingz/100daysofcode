ce670bbb322b90437867fd07556a83be4e9ec1f9
Jovanni Henry <85208389+Kryptek-tingz@users.noreply.github.com>
2022-11-29 19:54:08 -0500

    Add files via upload
    day 1 of 100daysofcode

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let country = "united states"
+let continent = "north america"
+let population = "2.1b"
+
+console.log(country)
+console.log(continent)
+console.log(population)
+*/
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun)
+
+console.log(typeof true)
